f5e69a3ba6ab0e5cc990500f4673391e442abfd3
JadeKro <carolinavalencia.cv801@gmail.com>
2023-10-28 15:04:36 -0500

    28/10/2023 15:05

--- maint.php
+++ maint.php
@@ -1,14 +1,21 @@
 <?php
-    $name= $_POST ['name'];
-    $lastname= $_POST ['lastname'];
-    $email= $_POST ['email'];
-    $cell= $_POST ['cell'];
-    $equip= $_POST['equip'];
-    $damage= $_POST['damage'];
 
-    $conexion= @mysqli_conection ($name, $lastname, $email, $cell, $equip, $damage);
+include("conexion.php");
 
-    if (!$conexion) { 
-        die('<strong>No pudo conectarse:</strong> ' . mysql_error());} 
+$name = $_POST['name'];
+$lastname = $_POST['lastname'];
+$email = $_POST['email'];
+$cell = $_POST['cell'];
+$equip = $_POST['equip'];
+$damage = $_POST['damage'];
 
-?>
+$sql = "INSERT INTO maint (name,lastname,email,cell,equip,damage)
+VALUES ('$name', '$lastname', '$email', '$cell', '$equip', '$damage')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
